ed00d290f5f53ab5a938d53878a5c6f490a9ff11
sershishkov <ser.shishkov@gmail.com>
2022-04-01 16:38:27 +0300

    136

--- App.jsx
+++ App.jsx
@@ -64,7 +64,14 @@ export default function App() {
     <>
       <StatusBar style='light' />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: GlobalStyles.colors.primary500,
+            },
+            headerTintColor: 'white',
+          }}
+        >
           <Stack.Screen
             name='ExpensesOverView'
             component={ExpensesOverView}
@@ -72,7 +79,13 @@ export default function App() {
               headerShown: false,
             }}
           />
-          <Stack.Screen name='ManageExpense' component={ManageExpense} />
+          <Stack.Screen
+            name='ManageExpense'
+            component={ManageExpense}
+            options={{
+              presentation: 'modal',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
