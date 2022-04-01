742329b50e280c3b3fe9707a17d0c9a8e35a19e5
sershishkov <ser.shishkov@gmail.com>
2022-04-01 18:55:35 +0300

    141

--- components/expensesOutput/ExpensesOutput.jsx
+++ components/expensesOutput/ExpensesOutput.jsx
@@ -4,69 +4,6 @@ import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 import { GlobalStyles } from '../../constants/styles';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoose',
-    amount: 58.99,
-    date: new Date('2021-12-22'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 88.99,
-    date: new Date('2022-01-18'),
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-02'),
-  },
-  {
-    id: 'e4',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19'),
-  },
-  {
-    id: 'e5',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2022-02-18'),
-  },
-  {
-    id: 'e6',
-    description: 'A pair of shoose',
-    amount: 58.99,
-    date: new Date('2021-12-22'),
-  },
-  {
-    id: 'e7',
-    description: 'A pair of trousers',
-    amount: 88.99,
-    date: new Date('2022-01-18'),
-  },
-  {
-    id: 'e8',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-02'),
-  },
-  {
-    id: 'e9',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19'),
-  },
-  {
-    id: 'e10',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2022-02-18'),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
